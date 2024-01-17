e9622a7666b9a243d14c49ad29b09f3c7de15969
qbobl5 <ddddd991125@gmail.com>
2024-01-17 19:18:49 +0900

    Initial commit

---
+++ src/test/java/com/koi/thejieun/ThejieunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koi.thejieun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThejieunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

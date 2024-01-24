b31a461bc8a6a43bacfd56e1c50880a60b649926
qbobl5 <ddddd991125@gmail.com>
2024-01-24 20:11:35 +0900

    feat: notice method 생성

--- src/main/java/com/koi/thejieun/notice/entity/Notice.java
+++ src/main/java/com/koi/thejieun/notice/entity/Notice.java
@@ -1,5 +1,6 @@
 package com.koi.thejieun.notice.entity;
 
+import com.koi.thejieun.notice.dto.NoticeDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,21 +26,27 @@ import java.util.Date;
 /* 공지사항 Entity */
 public class Notice {
     @Id
-    @Column(name="notice_no")
+    @Column(name = "notice_no")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "notice_no_seq_generator")
     // [PK] 공지사항 번호
     private Long noticeNo;
 
-    @Column(name="notice_title")
+    @Column(name = "notice_title")
     // 공지사항 제목
     private String noticeTitle;
 
-    @Column(name="notice_content")
+    @Column(name = "notice_content")
     // 공지사항 내용
     private String noticeContent;
 
-    @Column(name="notice_regdate")
-    @ColumnDefault(value="CURRENT_DATE")
+    @Column(name = "notice_regdate")
+    @ColumnDefault(value = "CURRENT_DATE")
     // 공지사항 생성일
     private Date noticeRegdate;
+
+    // 공지사항 수정 메소드
+    public void modifyNotice(NoticeDTO noticeDTO) {
+        this.noticeTitle = noticeDTO.getNoticeTitle();
+        this.noticeContent = noticeDTO.getNoticeContent();
+    }
 }

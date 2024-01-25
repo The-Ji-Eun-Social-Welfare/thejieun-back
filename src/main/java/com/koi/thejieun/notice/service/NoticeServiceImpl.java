c87c2d90761253ebe4c47586a69c7d3651919e2e
qbobl5 <ddddd991125@gmail.com>
2024-01-25 20:42:41 +0900

    feat: Notice

--- src/main/java/com/koi/thejieun/notice/service/NoticeServiceImpl.java
+++ src/main/java/com/koi/thejieun/notice/service/NoticeServiceImpl.java
@@ -8,6 +8,9 @@ import com.koi.thejieun.util.PageGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,12 +29,47 @@ public class NoticeServiceImpl implements NoticeService {
 
     @Override
     public PageGroup<NoticeDTO> findNoticeList(Integer currentPage) throws MyException {
-        return null;
+        if (currentPage < 1) currentPage = 1;
+        int cntPerPage = 10;
+
+        int start;
+        int end;
+        end = currentPage * cntPerPage;
+        start = (currentPage - 1) * cntPerPage + 1;
+
+        List<NoticeDTO> noticeDTOList = new ArrayList<>();
+        List<Object[]> objects=repository.findNoticeList(start, end);
+        Long noticeCnt = repository.count();
+
+        for (Object[] objArr : objects) {
+            NoticeDTO noticeDTO =
+                    NoticeDTO.builder()
+                            .noticeNo(Long.valueOf(String.valueOf(objArr[1])))
+                            .noticeTitle(String.valueOf(objArr[2]))
+                            .noticeContent(String.valueOf(objArr[3]))
+                            .noticeRegdate((Date)objArr[4])
+                            .build();
+            noticeDTOList.add(noticeDTO);
+        }
+
+        PageGroup<NoticeDTO> noticeDTOPageGroup =
+                new PageGroup<>(noticeDTOList, currentPage, noticeCnt);
+        return noticeDTOPageGroup;
     }
 
     @Override
     public NoticeDTO findByNoticeNo(Long noticeNo) throws MyException {
-        return null;
+        Optional<Notice> noticeOpt = repository.findById(noticeNo);
+        Notice notice = noticeOpt.get();
+        NoticeDTO noticeDTO =
+                NoticeDTO.builder()
+                        .noticeNo(notice.getNoticeNo())
+                        .noticeTitle(notice.getNoticeTitle())
+                        .noticeContent(notice.getNoticeContent())
+                        .noticeRegdate(notice.getNoticeRegdate())
+                        .build();
+
+        return noticeDTO;
     }
 
     @Override

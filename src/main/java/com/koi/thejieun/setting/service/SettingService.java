7d0f6f809a9e759433b464fb129d6c443ad85c81
qbobl5 <ddddd991125@gmail.com>
2024-01-25 20:52:46 +0900

    fix: 로그인 기능 인터페이스에 추가

--- src/main/java/com/koi/thejieun/setting/service/SettingService.java
+++ src/main/java/com/koi/thejieun/setting/service/SettingService.java
@@ -6,13 +6,24 @@ import com.koi.thejieun.setting.dto.SettingDTO;
 interface SettingService {
     /**
      * 관리자를 추가한다.
+     *
      * @param settingDTO 관리자 dto
      * @throws MyException
      */
     public void addSetting(SettingDTO settingDTO) throws MyException;
 
     /**
+     * settingNo에 해당하는 관리자를 조회한다.
+     *
+     * @param settingNo 번호
+     * @return settingNo에 해당하는 관리자
+     * @throws MyException
+     */
+    public SettingDTO findBySettingNo(Long settingNo) throws MyException;
+
+    /**
      * 관리자 정보를 수정한다.
+     *
      * @param settingDTO
      * @throws MyException
      */
